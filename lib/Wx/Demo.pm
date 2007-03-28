bae553b061fb85d7dca9eb70a3e6549dcfde7a66
mbarbon <mbarbon@2e9104e8-d5c8-4c90-bb67-bfd1792b7337>
2007-03-28 20:54:21 +0000

    Update copyright year.

--- lib/Wx/Demo.pm
+++ lib/Wx/Demo.pm
@@ -108,7 +108,7 @@ sub on_about {
     my( $self ) = @_;
     use Wx qw(wxOK wxCENTRE wxVERSION_STRING);
 
-    Wx::MessageBox( "wxPerl demo, (c) 2001-2006 Mattia Barbon\n" .
+    Wx::MessageBox( "wxPerl demo, (c) 2001-2007 Mattia Barbon\n" .
                     "wxPerl $Wx::VERSION, " . wxVERSION_STRING,
                     "About wxPerl demo", wxOK|wxCENTRE, $self );
 }
