865d77232f0da2293748807f6741d92b79c9362c
mbarbon <mbarbon@2e9104e8-d5c8-4c90-bb67-bfd1792b7337>
2008-06-29 20:00:22 +0000

    Starting release 0.11.

--- bin/wxperl_demo.pl
+++ bin/wxperl_demo.pl
@@ -6,7 +6,7 @@
 ## Modified by:
 ## Created:     14/08/2006
 ## RCS-ID:      $Id$
-## Copyright:   (c) 2006-2007 Mattia Barbon
+## Copyright:   (c) 2006-2008 Mattia Barbon
 ## Licence:     This program is free software; you can redistribute it and/or
 ##              modify it under the same terms as Perl itself
 #############################################################################
@@ -16,7 +16,7 @@ use strict;
 use Wx::Demo;
 use Getopt::Long;
 
-our $VERSION = '0.10';
+our $VERSION = '0.11';
 
 GetOptions( 'show=s'   => \( my $module ),
             );
