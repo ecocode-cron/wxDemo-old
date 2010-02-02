c6790ccd5ea336691dd71da1dae49d804107c4d2
mdootson <mdootson@2e9104e8-d5c8-4c90-bb67-bfd1792b7337>
2010-02-02 01:10:30 +0000

    wxDemo - wxMAC requires explicit pen style to be set.

--- lib/Wx/DemoModules/wxPrinting.pm
+++ lib/Wx/DemoModules/wxPrinting.pm
@@ -226,6 +226,7 @@ sub new {
   $this->SetScrollbars( 1, 1, $x_size, $y_size );
   $this->SetBackgroundColour( wxWHITE );
   $this->SetCursor( Wx::Cursor->new( wxCURSOR_PENCIL ) );
+  eval { $this->{overlay} = Wx::Overlay->new };
 
   EVT_MOTION( $this, \&OnMouseMove );
   EVT_LEFT_DOWN( $this, \&OnButton );
@@ -250,7 +251,7 @@ sub OnDraw {
   $dc->DrawEllipse( $x_size - 50 - 20, 20, 50, 50 );
   $dc->DrawEllipse( $x_size - 50 - 20, $y_size - 50 - 20, 50, 50 );
 
-  $dc->SetPen( Wx::Pen->new( wxRED, 5, 0 ) );
+  $dc->SetPen( Wx::Pen->new( wxRED, 5, wxSOLID ) );
   # wxGTK does not like DrawLines in this context
   foreach my $i ( @{$this->{LINES}} ) {
     my $prev;
@@ -277,8 +278,16 @@ sub OnMouseMove {
 
   push @{$this->{CURRENT_LINE}}, [ $x, $y ];
   my $elems = @{$this->{CURRENT_LINE}};
+  
+  my $overlaydc;
+  if( $this->{overlay} ) {
+    $overlaydc = Wx::DCOverlay->new($this->{overlay}, $dc);
+    $dc->SetPen( Wx::Pen->new( wxGREEN, 5, wxSOLID ) );
+  } else {
+    $dc->SetPen( Wx::Pen->new( wxRED, 5, wxSOLID ) );
+  }
 
-  $dc->SetPen( Wx::Pen->new( wxRED, 5, 0 ) );
+  
   $dc->DrawLine( @{$this->{CURRENT_LINE}[$elems-2]},
                  @{$this->{CURRENT_LINE}[$elems-1]} );
 
@@ -296,12 +305,17 @@ sub OnButton {
     push @{$this->{CURRENT_LINE}}, [ $x, $y ];
     push @{$this->{LINES}}, $this->{CURRENT_LINE};
     $this->ReleaseMouse();
+    if( $this->{overlay} ) {
+       $this->{overlay}->Reset;
+       $this->Refresh;
+    }
+        
   } else {
     $this->{CURRENT_LINE} = [ [ $x, $y ] ];
     $this->CaptureMouse();
   }
 
-  $dc->SetPen( Wx::Pen->new( wxRED, 5, 0 ) );
+  $dc->SetPen( Wx::Pen->new( wxRED, 5, wxSOLID ) );
   $dc->DrawLine( $x, $y, $x, $y );
 }
 
